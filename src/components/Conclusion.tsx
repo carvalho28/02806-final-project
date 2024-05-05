a1a21457893e18c626173f2c646ba6f41babd9ea
carvalho28 <carvalhoo287@gmail.com>
2024-05-05 18:53:53 +0200

    fix: "

--- src/components/Conclusion.tsx
+++ src/components/Conclusion.tsx
@@ -17,26 +17,26 @@ export function Conclusion() {
             Forging the future
           </h2>
           <p className="mt-4 font-display text-2xl tracking-tight text-red-900">
-            As we conclude our exploration of Denmark's transformative energy
-            initiatives, it is evident how Denmark is charting a robust green
-            path not just within its own borders but influencing the global
-            dialogue on sustainability. From pioneering reductions in greenhouse
-            gas emissions to leading the charge in renewable energy adoption,
-            Denmark's journey represents a compelling blueprint for sustainable
-            development.
+            As we conclude our exploration of Denmark&apos;s transformative
+            energy initiatives, it is evident how Denmark is charting a robust
+            green path not just within its own borders but influencing the
+            global dialogue on sustainability. From pioneering reductions in
+            greenhouse gas emissions to leading the charge in renewable energy
+            adoption, Denmark&apos;s journey represents a compelling blueprint
+            for sustainable development.
           </p>
           <p className="mt-4 font-display text-2xl tracking-tight text-red-900">
             While challenges remain, particularly in managing per capita
-            emissions, Denmark's proactive approach serves as a beacon for other
-            nations. Its commitment to environmental innovation and policy
+            emissions, Denmark&apos;s proactive approach serves as a beacon for
+            other nations. Its commitment to environmental innovation and policy
             exemplifies how a focused green shift can lead to significant global
             impacts.
           </p>
           <p className="mt-4 font-display text-2xl tracking-tight text-red-900">
-            Denmark's story, then, is more than just a national success; it is a
-            beacon for the global <b>"Green Shift"</b> proving that the path to
-            sustainability is both viable and vital for the future of our
-            planet.
+            Denmark&apos;s story, then, is more than just a national success; it
+            is a beacon for the global <b>&quot;Green Shift&quot;</b> proving
+            that the path to sustainability is both viable and vital for the
+            future of our planet.
           </p>
         </div>
         {/* line */}
